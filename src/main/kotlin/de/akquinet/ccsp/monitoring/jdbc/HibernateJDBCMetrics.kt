228d6f43e6590e5761e32cb128835c01c1e9e3e6
Markus Dahm <markus.dahm@akquinet.de>
2022-12-01 16:30:55 +0100

    enabled

--- src/main/kotlin/de/akquinet/ccsp/monitoring/jdbc/HibernateJDBCMetrics.kt
+++ src/main/kotlin/de/akquinet/ccsp/monitoring/jdbc/HibernateJDBCMetrics.kt
@@ -4,19 +4,25 @@ import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.binder.MeterBinder
 import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider
 import java.sql.Connection
+import java.sql.SQLException
 
 open class HibernateJDBCMetrics(private val connectionProvider: ConnectionProvider) : MeterBinder, ConnectionListener,
     ConnectionProvider by connectionProvider {
+    var enabled: Boolean = true
 
     override fun bindTo(registry: MeterRegistry) {
         // TODO
     }
 
-    override fun getConnection(): Connection {
+    @kotlin.jvm.Throws(SQLException::class)
+    final override fun getConnection(): Connection {
         val connection = connectionProvider.connection
 
         onOpenConnection(connection)
 
-        return connection
+        return if (enabled)
+            connection
+        else
+            connection
     }
 }
