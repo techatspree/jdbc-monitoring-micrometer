d37d3d9a5f3f3de8ef8a55737f8724b5dd9e93bf
Markus Dahm <markus.dahm@akquinet.de>
2022-12-14 14:51:30 +0100

    Configurable

--- src/main/kotlin/de/akquinet/ccsp/monitoring/jdbc/hibernate/HibernateConnectionProviderMetrics.kt
+++ src/main/kotlin/de/akquinet/ccsp/monitoring/jdbc/hibernate/HibernateConnectionProviderMetrics.kt
@@ -2,17 +2,38 @@ package de.akquinet.ccsp.monitoring.jdbc.hibernate
 
 import de.akquinet.ccsp.monitoring.AbstractJDBCMetrics
 import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider
+import org.hibernate.service.spi.Configurable
+import org.hibernate.service.spi.Stoppable
 import java.sql.SQLException
 
 /**
  * Use {@link org.hibernate.engine.jdbc.connections.spi.ConnectionProvider} to obtain connections
  */
+@Suppress("MemberVisibilityCanBePrivate")
 abstract class HibernateConnectionProviderMetrics(private val connectionProvider: ConnectionProvider) :
-	AbstractJDBCMetrics(), ConnectionProvider by connectionProvider {
+	AbstractJDBCMetrics(), ConnectionProvider by connectionProvider, Configurable, Stoppable {
+	protected val configurationValues: MutableMap<String, String> = HashMap()
+
 	@kotlin.jvm.Throws(SQLException::class)
 	@Suppress("UsePropertyAccessSyntax")
 	final override fun getConnection() = handleConnection(connectionProvider.getConnection())
 
 	@Suppress("UNCHECKED_CAST")
 	fun <P : ConnectionProvider> provider(): P = connectionProvider as P
+
+	final override fun configure(configurationValues: Map<Any?, Any?>) {
+		if (connectionProvider is Configurable) {
+			connectionProvider.configure(configurationValues)
+		}
+
+		for (entry in configurationValues) {
+			this.configurationValues[entry.key as String] = entry.value as String
+		}
+	}
+
+	final override fun stop() {
+		if (connectionProvider is Stoppable) {
+			connectionProvider.stop()
+		}
+	}
 }
